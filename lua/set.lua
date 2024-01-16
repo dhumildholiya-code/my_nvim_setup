eea2b5957d0f0b8540ffd06c504278f577fdecb1
Dhumil Dholiya <dhumildholiya97@gmail.com>
2024-01-16 22:25:56 +0530

    added some other set values.

--- lua/set.lua
+++ lua/set.lua
@@ -2,6 +2,7 @@ vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -18,11 +19,14 @@ vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
+vim.opt.wildmenu = true
+vim.opt.wildmode = {'longest:full'}
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 10 
 
 vim.opt.updatetime = 50
 
