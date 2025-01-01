f2c6fb1bc9fa6a868fc9d6777e8402f5fa8ae064
dhumildholiya97 <dhumildholiya97@gmail.com>
2025-01-01 12:17:07 +0530

    added build_quickfix_traversal and terminal

--- lua/dhumil/init.lua
+++ lua/dhumil/init.lua
@@ -35,30 +35,6 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 
-vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set("n", "<F5>", ":!build.bat<CR>")
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
 --NOTE(Dhumil): Jblow colorscheme
 vim.cmd [[hi normal guibg=#072626]]
 vim.cmd [[hi @keyword guifg=#FFFFFF]]
@@ -78,3 +54,67 @@ vim.cmd [[hi @character guifg=#0FDFAF]]
 vim.cmd [[hi @boolean guifg=#7FFFD4]]
 vim.cmd [[hi @operator guifg=#D3B58D]]
 vim.cmd [[hi Visual guibg=#0000FF]]
+
+vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<F5>", ":lua Build()<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<M-j>', "<cmd>cnext<CR>")
+vim.keymap.set('n', '<M-k>', "<cmd>cprev<CR>")
+
+vim.keymap.set("n", "<leader>t", function()
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
+end)
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd('TermOpen', {
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
+})
+
+function Build()
+    vim.cmd("cclose")
+    local test = vim.fn.system("build.bat")
+    local list = {}
+
+    local file, line, col, msg = string.match(test, "(.*)%((%d+):(%d+)%) (.*)")
+    if file and line and col and msg then
+        table.insert(list, {
+            filename = file,
+            lnum = tonumber(line),
+            col = tonumber(col),
+            text = msg,
+        })
+    end
+    if #list > 0 then
+        vim.fn.setqflist(list, 'r')
+        vim.cmd("copen")
+    else
+        print("Compile Successfully!")
+    end
+end
