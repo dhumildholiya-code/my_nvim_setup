cdb3d7453eb255db69eea91f2b5c610d69c231b6
dhumildholiya97 <dhumildholiya97@gmail.com>
2025-01-01 15:05:34 +0530

    remap telescope keymap and added register support

--- lua/dhumil/init.lua
+++ lua/dhumil/init.lua
@@ -65,14 +65,19 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 vim.keymap.set('n', '<M-j>', "<cmd>cnext<CR>")
 vim.keymap.set('n', '<M-k>', "<cmd>cprev<CR>")
+
+--NOTE(Dhumil): Greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
 
 vim.keymap.set("n", "<leader>t", function()
     vim.cmd.vnew()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,9 +16,9 @@ require('lazy').setup({
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require "telescope.builtin"
-            vim.keymap.set("n", "<leader>pg", builtin.git_files)
-            vim.keymap.set("n", "<leader>pf", builtin.find_files)
-            vim.keymap.set("n", "<leader>ps", builtin.grep_string)
+            vim.keymap.set("n", "<leader>sg", builtin.git_files)
+            vim.keymap.set("n", "<leader>sf", builtin.find_files)
+            vim.keymap.set("n", "<leader>ss", builtin.grep_string)
             vim.keymap.set("n", "<leader>g", builtin.live_grep)
             vim.keymap.set("n", "<leader><leader>", builtin.buffers)
 
